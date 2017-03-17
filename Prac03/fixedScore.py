65050d7eb6979a003be71c42ae6a233a7d07bb91
Ally Smith <aliesha.smith@my.jcu.edu.au>
2017-03-17 13:53:19 +1000

    Add functions to fixedScore.py.

--- Prac03/fixedScore.py
+++ Prac03/fixedScore.py
@@ -3,15 +3,23 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score > 90:
-    print("Excellent")
-elif score > 50:
-    print("Passable")
-else:
-    print("Bad")
 
+def main():
+    score = float(input("Enter score: "))
+    grade = grade_calculator(score)
+    print(grade)
+
+
+def grade_calculator(score):
+    if score < 0 or score > 100:
+        grade = ("Invalid score")
+    elif score > 90:
+        grade = ("Excellent")
+    elif score > 50:
+        grade = ("Passable")
+    else:
+        grade = ("Bad")
+    return grade
+
+
+main()
