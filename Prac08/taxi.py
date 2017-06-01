f9b7c08a0c9c02da41becd462bebbc1aaa282536
Ally Smith <aliesha.smith@my.jcu.edu.au>
2017-06-01 10:13:32 +1000

    Complete SilverServiceTaxi Exercise.

--- Prac08/taxi.py
+++ Prac08/taxi.py
@@ -73,3 +73,17 @@ class UnreliableCar (Car):
             distance_driven = super().drive(distance)
             return distance_driven
 
+
+class SilverServiceTaxi(Taxi):
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = float(fanciness)
+        self.price_per_km = Taxi.price_per_km * self.fanciness
+
+    def __str__(self):
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
--- Prac08/test_taxi.py
+++ Prac08/test_taxi.py
@@ -1,16 +1,17 @@
 from Prac08.taxi import Taxi
 from Prac08.taxi import UnreliableCar
+from Prac08.taxi import SilverServiceTaxi
 
 # Test Taxi Class
-prius1 = Taxi('Prius 1', 100)
-prius1.drive(40)
-print(prius1)
-print(prius1.get_fare())
+prius = Taxi('Prius 1', 100)
+prius.drive(40)
+print(prius)
+print(prius.get_fare())
 
-prius1.start_fare()
-prius1.drive(100)
-print(prius1)
-print(prius1.get_fare())
+prius.start_fare()
+prius.drive(100)
+print(prius)
+print(prius.get_fare())
 print()
 
 # Test Unreliable Car Class
@@ -18,3 +19,9 @@ ford = UnreliableCar('Ford', 100, 50)
 ford.drive(40)
 print(ford)
 print()
+
+# Test Silver Service Taxi Class
+hummer = SilverServiceTaxi('Hummer', 200, 2)
+hummer.drive(10)
+print(hummer)
+print('${:.2f}'.format(hummer.get_fare()))
